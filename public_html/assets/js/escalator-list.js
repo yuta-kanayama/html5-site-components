187a76af415e4c7bfbdd6d035089413e4456c75e
Yuta Kanayama <kanayama.work@gmail.com>
2014-10-23 20:13:22 +0900

    escalator list

--- public_html/assets/js/escalator-list.js
+++ public_html/assets/js/escalator-list.js
@@ -31,7 +31,12 @@ EscalatorList.prototype = {
 		}
 		for(var key in opt) self.prm[key] = opt[key];
 		
+		self.data;
 		self.timer;
+		self.count = 0;
+		self.countMax = 0;
+		self.interval = 1000;
+		
 		self.$target = self.prm.target;
 		self.$item;
 		
@@ -45,55 +50,59 @@ EscalatorList.prototype = {
 		self.setEvent();
 		
 		self.loadJSON()
-			.then( function( json ) {
-				console.log( json );
+			.then( function() {
 				
-				var html = '',
-						dom,
-						count = 0,
-						max = json.length;
+				var dom,
+						max = 5;
+				self.countMax = self.data.length;
 				
-				if( 5 < max ) max = 5;
+				if( 5 > self.countMax ) max = self.countMax;
 				
 				for( var i = 0; i < max; i++ ) {
-					dom = self.createDOM( json[i] );
-					console.log( dom );
+					dom = self.createDOM( self.data[i] );
 					self.$target.prepend( dom );
-					//html += self.createHTML( json[i] );
 				}
-				count = max;
-				//self.$target.html( html );
+				self.count = max;
 				
-				self.timer = setInterval( function() {
-					dom = self.createDOM( json[i] );
-					console.log( dom );
-					dom.find('.thumbnail img').load( function( event ) {
-						//console.log( event );
-						self.$target.prepend( dom );
-						var dom_wrapper = dom.find('.l-wrapper');
-						var dom_inner = dom.find('.l-inner');
-						dom_wrapper.height( dom.height() );
-						dom.slideUp(0);
-						dom_inner.fadeOut(0);
-						dom.slideDown(1000, function() {
-							dom_inner.fadeIn(1000);
-						});
-					} );
-					count++;
-					if( count >= json.length ) count = 0;
-				}, 3000 );
+				self.timer = setTimeout( $.proxy( self.animation, self ), self.interval );
 				
 			} );
 		
 	},
 	
 	
+	animation: function() {
+		var self = this;
+		clearTimeout( self.timer );
+		
+		var dom = self.createDOM( self.data[ self.count ] );
+		//console.log( dom );
+		dom.find('.thumbnail img').load( function( event ) {
+			
+			self.$target.height( self.$target.height() );
+			
+			self.$target.prepend( dom );
+			var dom_wrapper = dom.find('.l-wrapper');
+			var dom_inner = dom.find('.l-inner');
+			dom_wrapper.height( dom.height() );
+			dom_inner.fadeOut(0);
+			dom.slideUp(0)
+			.slideDown(1000, function() {
+				dom_inner.fadeIn(1000, function() {
+					self.count++;
+					if( self.count >= self.countMax ) self.count = 0;
+					self.timer = setTimeout( $.proxy( self.animation, self ), self.interval );
+				});
+				self.$target.find('.escalator-list__item:last').remove();
+			});
+		} );
+	},
+	
+	
 	setEvent: function() {
 		var self = this;
 		
-		self.$target.load( function() {
-			console.log( 'target.load' );
-		} );
+		
 	},
 	
 	
@@ -107,7 +116,8 @@ EscalatorList.prototype = {
 			dataType: 'json',
 			cache: false,
 			success: function( json ) {
-				dfd.resolve( json );
+				self.data = json;
+				dfd.resolve();
 			},
 			error: function() {
 				console.log('error');
@@ -123,23 +133,21 @@ EscalatorList.prototype = {
 		var self = this,
 				html = '';
 		
-		//html += '<li class="escalator-list__item">';
-		html += 	'<section class="l-wrapper">';
-		html += 		'<a href="'+ data.permalink +'" class="l-inner">';
-		html += 			'<div class="l-primary">';
-		html += 				'<figure class="thumbnail">';
-		html += 					'<img src="'+ data.thumbnail +'" alt="" />';
-		html += 				'</figure>';
-		html += 			'</div>';
-		html += 			'<div class="l-secondary">';
-		html += 				'<h1 class="title">'+ data.title + '</h1>';
-		html += 				'<p class="text">'+ data.text +'</p>';
-		html += 			'</div>';
-		html += 		'</a>';
-		html += 	'</section>';
-		//html += '</li>';
+		html += '<div class="l-wrapper">';
+		html += 	'<a href="'+ data.permalink +'" class="l-inner">';
+		html += 		'<div class="l-primary">';
+		html += 			'<figure class="thumbnail">';
+		html += 				'<img src="'+ data.thumbnail +'" alt="" />';
+		html += 			'</figure>';
+		html += 		'</div>';
+		html += 		'<div class="l-secondary">';
+		html += 			'<h1 class="title">'+ data.title + '</h1>';
+		html += 			'<p class="text">'+ data.text +'</p>';
+		html += 		'</div>';
+		html += 	'</a>';
+		html += '</div>';
 		
-		var dom = $('<li>', {
+		var dom = $('<section>', {
 			'class': 'escalator-list__item'
 		}).html( html );
 		
